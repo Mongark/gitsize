79d9c86108ad56b2346f10b2d981fab425212c8b
Mongark <mongarkprogrammer@gmail.com>
2022-12-12 11:44:28 -0400

    Added crates 'serde' and 'github-types'

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 fn get_data() {
     let url = "https://api.github.com/repos/denoland/deno".to_string();
     let client = reqwest::blocking::Client::new();
-    let data: _ = client
+    let raw_data: _ = client
         .get(url)
         .header("User-Agent", "getsize")
         .send()
