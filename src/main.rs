eb83d9eeefbe8f7e59966ba0c6bf1f4eab4d5683
Mongark <mongarkprogrammer@gmail.com>
2022-12-12 11:20:49 -0400

    First semi-successful test for API calls

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::env;
 
-// default configuration parameters
-// const HUMAN_READABLE: bool = false;
+fn get_metadata() {
+    let url = "https://api.github.com/repos/denoland/deno".to_string();
+
+    let data = reqwest::blocking::get(url).unwrap().text();
+
+    dbg!(data);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    get_metadata();
 }
