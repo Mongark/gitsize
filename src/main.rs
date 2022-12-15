179411406825128bc27d4ca5bdfce1bc228a99ed
Mongark <mongarkprogrammer@gmail.com>
2022-12-15 14:34:24 -0400

    Refactored 'get_size_from'

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::env;
 
+use humansize::{format_size, DECIMAL};
 use serde_json::Value;
-use humansize::{ format_size, DECIMAL };
 
 fn get_size_from(target: String) -> String {
-    let url = "https://api.github.com/repos/".to_string()+&target;
+    let url = "https://api.github.com/repos/".to_string() + &target;
     let client = reqwest::blocking::Client::new();
 
     let raw_data = client
@@ -13,17 +13,15 @@ fn get_size_from(target: String) -> String {
         .send()
         .expect("Failed to send message to GitHub API. Try again");
 
-    let text_data = raw_data
-        .text()
-        .unwrap();
+    let text_data = raw_data.text().unwrap();
 
-    let data: Value = serde_json::from_str(&text_data)
-        .expect("Error converting data");
+    let data: Value = serde_json::from_str(&text_data).expect("Error converting data");
 
     let raw_size: u64 = data["size"]
         .clone()
         .as_u64()
-        .expect("Erro extracting size from JSON data") * 1000;
+        .expect("Erro extracting size from JSON data")
+        * 1000;
 
     let formated: String = format_size(raw_size, DECIMAL);
 
